ac89006e875c835e5571008e3363585b143b5992
KGallyamov <mirakpjcppr@gmail.com>
2022-06-08 17:13:23 +0300

    Delete intermediate files before exiting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from scraping.parse import Scraper
 
 if __name__ == '__main__':
-    scraper = Scraper("Path to Chrome driver folder")
-    scraper._get_general_university_stats()  # 16679109100
-    pass
+    scraper = Scraper("Path to Chrome driver")
+    scraper._get_authors_list()
+    with open('scraping/data.html', 'w') as file:
+        file.truncate(0)

d4824d6a647f225629f360cf2d4d9dae0f45044f
KGallyamov <mirakpjcppr@gmail.com>
2022-06-16 16:51:07 +0300

    This closes #10

--- scraping/parse.py
+++ scraping/parse.py
@@ -1,187 +1,125 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-import time
-from calendar import month_name
+import pandas as pd
+from ElsapyCustom.elsclient import *
+from ElsapyCustom.elsprofile import *
+from ElsapyCustom.elssearch import *
+from ElsapyCustom.elsdoc import *
 
 
 class Scraper:
-    def __init__(self, driver_folder, headless=False):
-        self.authors = dict()  # (author id) -> (full data on author)
-        self.author_to_paper = dict()  # (person id) -> [(paper id)]
-        self.papers = dict()  # (paper id) -> (full data on paper)
+    def __init__(self):
+        with open('scraping/key.txt', 'r') as file:
+            key = file.readline()
+        self.client = ElsClient(key)
 
-        self.authors_num = 0
-        self.papers_num = 0
-        self.subjects = dict()  # (Area of research) -> (Number of articles)
+    def parse(self, preload=True):
+        print('Scraper', id(self), 'began parsing')
+        if preload:
+            papers = pd.read_csv('data/papers.csv')
+        else:
+            papers = self._get_papers()
+        paper_ids = list(papers['id'])
+        author_ids = set()
+        paper_authors_affils_data = dict()
+        papers_citations = dict()
+        overall_it = len(paper_ids)
+        for it, paper_id in enumerate(paper_ids):
+            print('Parsing article #', it + 1, 'out of', overall_it)
+            paper_authors_affils_data[paper_id], innopolis_authors = self._get_paper_affils(paper_id)
+            author_ids |= innopolis_authors
+            papers_citations[paper_id] = self._get_paper_citations(paper_id)
+        paper_affils_df = pd.DataFrame.from_dict({'id': paper_authors_affils_data.keys(),
+                                                  'authors_affils': paper_authors_affils_data.values()})
 
-        path = driver_folder + "chromedriver.exe"
-        options = Options()
-        options.headless = headless
-        self.driver = webdriver.Chrome(path, options=options)
-        self.driver.implicitly_wait(5)
-        print("Scraper " + str(id(self)) + " created")
-        with open('scraping/data.html'):
-            pass
+        papers = pd.merge(papers, paper_affils_df, on='id', how="left")
+        papers.to_csv('data/new_papers.csv', index=False)
 
-    def parse(self):
-        self._get_general_university_stats()
-        self._get_papers_authors()
+        haha = 0
 
-    # Returns a list of scopus ids
-    def _get_authors_list(self):
-        print("Scraper " + str(id(self)) + " parses university workers")
-        url = "https://www.scopus.com/results/authorNamesList.uri?affiliationId=60105869&" \
-              "s=AF--ID%2860105869%29&resultsPerPage=500"
-        self.driver.get(url)
-        text = self.driver.page_source
-        self.driver.quit()
-        with open('scraping/data.html', 'w', encoding='utf-8') as file:
-            file.write(text)
+        authors = dict()
+        for author_id in author_ids:
+            authors[author_id] = self._get_author_by_id(author_id)
 
-    def _get_author_data(self, author_id):
-        print("Scraper " + str(id(self)) + " parses author " + str(author_id))
-        self.driver.get("https://www.scopus.com/authid/detail.uri?authorId=" + str(author_id))
-        data = self.driver.page_source
-        self.driver.quit()
-        with open('scraping/data.html', 'w', encoding='utf-8') as file:
-            file.write(data)
+        return papers, paper_authors_affils_data, authors
 
-    '''Number of workers, works and distribution of the latter among topics'''
+    def _get_paper_affils(self, scopus_id):
+        print('Scraper', id(self), 'parses document', str(scopus_id))
+        paper = AbsDoc(scp_id=str(scopus_id))
+        paper.read(self.client)
+        authors_response = paper.data['authors']['author']
 
-    def _get_general_university_stats(self):
-        print("Scraper " + str(id(self)) + " parses university general statistics")
+        affils_names = dict()
+        affils_responses = paper.data['affiliation']
+        if isinstance(affils_responses, dict):
+            affils_responses = [affils_responses]
 
-        url = "https://www.scopus.com/affil/profile.uri?afid=60105869"
-        self.driver.get(url)
+        for affils_response in affils_responses:
+            affils_names[affils_response['@id']] = affils_response['affilname']
 
-        subject_distribution = self.driver.find_element(By.ID, "subjectList").text.split('\n')
-        affiliation_general = self.driver.find_element(By.ID, "affilAddlSec").text.split('\n')
+        author_affils = dict()
+        innopolis_affiliated = set()
+        for response in authors_response:
+            affils = response['affiliation']
+            if not isinstance(affils, list):
+                affils = [affils]
+            author_affils[response['@auid']] = list()
+            for affil in affils:
+                author_affils[response['@auid']].append(affils_names[affil['@id']])
+                if affil['@id'] == '60105869':
+                    innopolis_affiliated.add(response['@auid'])
 
-        self.papers_num = int(''.join(affiliation_general[1].split(' ')))  # The line is stored like '1 225', not '1225'
-        self.authors_num = int(affiliation_general[3])
+        # if len(innopolis_affiliated) == 0:
+        #     pass
+        #     # Do not actually know what to do in this case
+        return author_affils, innopolis_affiliated
 
-        for ind in range(0, len(subject_distribution), 2):
-            self.subjects[subject_distribution[ind]] = int(subject_distribution[ind + 1])
-
-        self.driver.quit()
-
-    def _get_papers(self, author_id):
-        url = "https://www.scopus.com/results/results.uri?sort=plf-f&src=s&sid=c1140baa70c949831487b6121ec26b3d&sot=aff&sdt=a&sl=15&s=AF-ID%2860105869%29&origin=AffiliationProfile&editSaveSearch=&txGid=2b6475e16d6694003cc51e4df73b6ae1"
-
-    # Parses general information on a selected work: name, list of authors, citations, etc
-    def _get_paper_data(self, paper_id):
-        print("Scraper " + str(id(self)) + " parses article " + str(paper_id))
-        url = "https://www.scopus.com/record/display.uri?eid=2-s2.0-" + \
-              str(paper_id) + "&origin=resultslist&featureToggles=FEATURE_NEW_DOC_DETAILS_EXPORT:1"
-
-        self.driver.get(url)
-        paper_name = self.driver.find_element(By.CLASS_NAME, 'Highlight-module__1p2SO').text
-        authors = self._get_papers_authors()
-        overall_citations = 0
-        work_percentile = 100
-        fwci = 0
-        # An article may not yet have any citations
-        try:
-            citations_info = self.driver.find_element(By.XPATH, '//els-info-field[@href="#metrics"]')
-            overall_citations = int(citations_info.get_attribute("value"))
-            work_percentile = citations_info.get_attribute("metavalue")
-            fwci = self.driver.find_elements(By.CLASS_NAME, 'vertical-highlight.sc-els-info-field')[1]. \
-                find_element(By.CLASS_NAME, 'sc-els-info-field.hydrated').text
-        except:
-            pass
-
-        source_info = self.driver.find_element(By.CLASS_NAME,
-                                               'PublicationInformationBar-module__2SO0m.padding-size-8-t'). \
-            find_elements(By.CLASS_NAME, 'hydrated')
-        source_name = source_info[0].text  # name of a journal/conference/publisher
-
-        months = {m.lower() for m in month_name[1:]}
-        # Date of publishing does not have a static place
-        publishing_date = None
-        for info in source_info:
-            if any([month in info.text.lower() for month in months]):
-                publishing_date = info.text
-                break
-
-        keywords = self.driver.find_elements(By.CLASS_NAME, 'margin-size-4-t')[-1].text.split('; ')
-
-        publishing_data = self.driver.find_elements(By.CLASS_NAME,
-                                                    'stack.stack--xxs.stack--mode-container.stack--vertical.'
-                                                    'stack--start.margin-size-0')[:4]
-        document_type = publishing_data[0].text.split('\n')[1]  # Is a work article or book
-        if '•' in document_type:
-            document_type = document_type.split('•')[0]
-        source_type = publishing_data[1].text.split('\n')[1]  # where the work was presented
-        issn_isbn = publishing_data[2].text.split('\n')[1]
-        if ',' in issn_isbn:
-            issn_isbn = issn_isbn.split(',')
-        doi = publishing_data[3].text.split('\n')[1]
-
-        citations = self._get_citation_dynamics(paper_id)
-
-        return (paper_name, authors, source_name, overall_citations, work_percentile, fwci,
-                publishing_date, keywords, document_type, source_type, issn_isbn, doi, citations)
-
-    # Returns list of article's authors and their affiliations
-    def _get_papers_authors(self):
-        authors_data_raw = self.driver.find_element(By.CLASS_NAME, 'ul--horizontal.margin-size-0').text.split('\n')
-
-        affiliations_raw = self.driver.find_element(By.ID, 'affiliation-section'). \
-            find_element(By.CLASS_NAME, 'margin-size-0').text.split('\n')
-
-        affiliations_encoding = dict()
-        for affiliation in affiliations_raw:
-            words = affiliation.split(' ')
-            affiliations_encoding[words[0]] = ''.join(words[1:])
-
-        names = []
-        affiliations = []
-        affiliations_enc = []
-        print(authors_data_raw)
-        '''
-        in "authors_data_raw" first entry has just the name of an author,
-        and following entry has the affiliations of the previous author
-        last entry does not contain author name 
-        '''
-        for it, author_data in enumerate(authors_data_raw):
-            if it == 0:
-                names.append(author_data)
-            elif ';' in author_data:
-                data = author_data.split('; ')
-                print(data)
-                names.append(data[1])
-                affiliations_enc.append(data[0])
-            else:
-                affiliations_enc.append(author_data)
-        print(dict(zip(names, affiliations_enc)))
-        return dict(zip(names, affiliations_enc))
-
-    # Parses the annual number of citations of a paper (2007-2022 so far)
-    def _get_citation_dynamics(self, paper_id):
-        print("Scraper " + str(id(self)) + " parses article " + str(paper_id) + "'s citation dynamics")
-
+    def _get_paper_citations(self, paper_id):
+        citation_data = self.client.exec_request('https://api.elsevier.com/content/abstract/citations/?scopus_id=' +
+                                                 str(paper_id) + '&date=2000-2022')
         citations = dict()
-        url = "https://www.scopus.com/record/pubmetrics.uri?eid=2-s2.0-" + str(paper_id) + "&origin=recordpage"
-        self.driver.get(url)
-        # Required for dynamic elements to load
-        time.sleep(2)
 
-        year_list = self.driver.find_element(By.CLASS_NAME,
-                                             'ui-selectmenu-icon.ui-icon.btn-primary.btn-icon.ico-navigate-'
-                                             'down.flexDisplay.flexAlignCenter.flexJustifyCenter.flexColumn')
-        # Choose the period from 2007-2022
-        year_list.click()
-        year = self.driver.find_element(By.ID, 'ui-id-17')
-        year.click()
-        show = self.driver.find_element(By.ID, 'buttonOn')
-        show.click()
+        for i in range(0, len(
+                citation_data['abstract-citations-response']['citeColumnTotalXML']['citeCountHeader']['columnTotal'])):
 
-        dots = self.driver.find_element(By.CLASS_NAME,
-                                        'highcharts-markers.highcharts-series-0.highcharts-line-'
-                                        'series.highcharts-color-0.default.highcharts-tracker')
-        dots = dots.find_elements(By.CLASS_NAME, 'highcharts-point.highcharts-color-0')
-        for dot in dots:
-            label = dot.get_attribute('aria-label').split('.')[1].split(',')
-            citations[int(label[0])] = int(label[1])
+            if int(citation_data['abstract-citations-response']['citeColumnTotalXML']['citeCountHeader']['columnTotal'][
+                       i]['$']) > 0:
+                citations[citation_data['abstract-citations-response']['citeColumnTotalXML']['citeCountHeader'][
+                    'columnHeading'][i]['$']] = \
+                    citation_data['abstract-citations-response']['citeColumnTotalXML']['citeCountHeader'][
+                        'columnTotal'][i][
+                        '$']
+
         return citations
+
+    def _get_author_by_id(self, scopus_id):
+        author = self.client.exec_request('https://api.elsevier.com/content/author/author_id/' + str(scopus_id))
+        return author
+
+    def _get_university_info(self):
+        info = ElsAffil(affil_id='60105869')
+        info.read(self.client)
+        return info
+
+    def _get_papers(self):
+        print('Scraper', id(self), 'parses list of all documents')
+        papers_response = ElsSearch("affil(Innopolis University)", 'scopus')
+        papers_response.execute(self.client, True)
+        papers = papers_response.results_df
+        papers = papers.drop(['@_fa', 'link', 'prism:url', 'eid', 'openaccess',
+                              'openaccessFlag', 'prism:volume', 'prism:pageRange',
+                              'subtype', 'pii', 'freetoread', 'freetoreadLabel',
+                              'prism:issn', 'prism:isbn', 'prism:eIssn',
+                              'prism:coverDisplayDate', 'pubmed-id', 'article-number',
+                              'prism:issueIdentifier', 'dc:creator', 'affiliation'],
+                             axis=1)
+        papers = papers.rename(columns={"dc:identifier": "id", "dc:title": "title",
+                                        "prism:publicationName": "publisher",
+                                        "prism:coverDate": "publication_date", "prism:doi": "doi",
+                                        "citedby-count": "overall_citations",
+                                        "prism:aggregationType": "source_type",
+                                        "subtypeDescription": "work_type"})
+        papers['id'] = papers['id'].apply(lambda x: x.split(':')[1])
+        papers['source-id'] = papers['source-id'].apply(lambda x:
+                                                        'Nan' if pd.isnull(x) else str(int(x)))
+
+        papers.to_csv('data/papers.csv', index=False)
+        return papers

15f83f10d4a17810e7d5a5210a5e39ea6e9a97a1
Григорий Рыболовлев <95378753+GrishaRybolovel@users.noreply.github.com>
2022-06-21 19:17:14 +0300

    Add files via upload

--- bibliometrics/api/views.py
+++ bibliometrics/api/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import FileResponse
+from .settings import TOKEN
 import time
 from datetime import datetime
-
+import asyncio
+import os
+import pathlib
 # Create your views here.
 
 def get(request, name, token):
-	try:
-		db = open('/Users/grisharybolovlev/IU/bibliometrics/bibliometrics/api/assets/' + str(name) + '.csv', 'rb')
+	if token == TOKEN:
+		try:
+			db = open(str(pathlib.Path(__file__).parent.resolve()) + '/assets/' + str(name) + '.csv', 'rb')
 
-		return FileResponse(db)
+			return FileResponse(db)
 
-	except:
-		return HttpResponse('No such file or your request is processing!')
-
-
+		except:
+			return HttpResponse('No such file or your request is processing!')
+	else:
+		return HttpResponse('Invalid token')
 
 def update(request, token):
-	now = datetime.now()
-	current_time = now.strftime("%Y/%m/%d-%H:%M:%S")
-	return HttpResponse('DB will be uploaded with name "' + current_time + '"')
+	if token == TOKEN:
+		now = datetime.now()
+		current_time = now.strftime("%Y/%m/%d-%H:%M:%S")
+		return HttpResponse('DB will be uploaded with name "' + current_time + '"')
+	else:
+		return HttpResponse('Invalid token')

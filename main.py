8cd02842162bcb45b6387a60f32e81f5644fdd59
VadigullinElmir1 <89780734+VadigullinElmir1@users.noreply.github.com>
2022-06-21 23:02:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from scraping.parse import Scraper
 from scraping.quartile import quartile_of
+from scraping.research_gate import members_of_Inno
 
 if __name__ == '__main__':
     scraper = Scraper()

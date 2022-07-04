585d904f42345302398a504230b5de1b130694fd
Григорий Рыболовлев <95378753+GrishaRybolovel@users.noreply.github.com>
2022-07-04 16:09:29 +0300

    Add files via upload

---
+++ scraping/main.py
@@ -0,0 +1,9 @@
+#from parse import Scraper
+#from quartile import quartile_of
+#from research_gate import members_of_Inno
+
+#scraper = Scraper()
+#scraper.parse()
+file = open("/Users/grisharybolovlev/IU/bib/biblio/scraping/output.txt", "a")
+file.write("Привет, как дела?")
+file.close()
